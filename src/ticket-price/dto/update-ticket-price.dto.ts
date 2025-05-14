64fa2790ca2a459bf5c3e1daa2296b9ad97ec7fd
Bill-GD <geometrydashbill@gmail.com>
2025-05-14 10:28:00 +0700

    Ticket price: Update ticket price
    Added manually commit from !16+

---
+++ src/ticket-price/dto/update-ticket-price.dto.ts
@@ -0,0 +1,46 @@
+import {
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  Min,
+  IsNotEmpty,
+  IsBoolean,
+} from 'class-validator';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
+import { SeatType, MovieType } from '../entities/ticket-price.entity';
+import { CreateTicketPriceDto } from './create-ticket-price.dto';
+
+export class UpdateTicketPriceDto extends PartialType(CreateTicketPriceDto) {
+  @ApiPropertyOptional({ enum: SeatType })
+  @IsOptional()
+  @IsEnum(SeatType)
+  type_seat?: SeatType;
+
+  @ApiPropertyOptional({ enum: MovieType })
+  @IsOptional()
+  @IsEnum(MovieType)
+  type_movie?: MovieType;
+
+  @ApiPropertyOptional({ example: 100000 })
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  price?: number;
+
+  @ApiPropertyOptional({
+    description: 'false - ngày thường; true - cuối tuần/ngày lễ',
+  })
+  @IsOptional()
+  @IsBoolean()
+  day_type?: boolean;
+
+  @ApiPropertyOptional({ example: '10:00:00' })
+  @IsOptional()
+  @IsNotEmpty()
+  start_time?: string;
+
+  @ApiPropertyOptional({ example: '14:00:00' })
+  @IsOptional()
+  @IsNotEmpty()
+  end_time?: string;
+}
