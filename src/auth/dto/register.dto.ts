33c915eed02cc432680911f65f77da6ca46650d9
Hồ Đức Thanh <thanhbr06@gmail.com>
2025-05-15 02:15:36 +0000

    Auth: Added missing swagger docs for login and register schema
    Addressed part of #17+

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,24 +1,31 @@
 import { IsEmail, IsNotEmpty, IsString, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterDto {
+  @ApiProperty({ example: 'John' })
   @IsNotEmpty()
   @IsString()
   first_name: string;
 
+  @ApiProperty({ example: 'Doe' })
   @IsNotEmpty()
   @IsString()
   last_name: string;
 
+  @ApiProperty({ example: 'JohnDoe@example.com' })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: '12345' })
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({ example: '0123456789' })
   @IsOptional()
   @IsString()
   phone?: string;
 
+  @ApiProperty({ example: 'Ha Noi' })
   @IsOptional()
   @IsString()
   address?: string;
