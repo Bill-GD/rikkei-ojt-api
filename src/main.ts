1e3b0675d45ded3d66890f6b8c69b1f775d44af2
Bill-GD <geometrydashbill@gmail.com>
2025-05-15 23:21:46 +0700

    removed validation pipe
    this causes validation errors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { CatchAllExceptionFilter } from './common/utils/exception.filter';
 
 async function bootstrap() {
@@ -17,13 +16,6 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, documentFactory);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
   app.useGlobalFilters(new CatchAllExceptionFilter(app.get(HttpAdapterHost)));
 
   await app.listen(process.env.PORT ?? 3000);
